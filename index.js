c2cf4faeec9d54fe197b8ec44171516bc3e3a844
Vladimir Sanchez <vladypuggu@gmail.com>
2025-02-03 19:53:30 -0600

    Created import for faker
    Created const products in /products route
    
    Created const size in /products route
    
    Created const limit in /products route
    
    Created metod "for"  in /products route
    
    Created /products/filter route
    
    Created /users route
    
    Created /categories route
    
    Created /categories/:id route
    
    Created /people route
    
    Created /people/:id route

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const express = require('express');
 const app = express();
 //Y declaramos el puerto
 const port = 3000;
+//importamos faker
+const { faker } = require('@faker-js/faker');
+
 
 app.get('/', (req, res) => {
   res.send('Hola mi server en express');
@@ -14,19 +17,25 @@ app.get('/nueva-ruta', (req, res) => {
 })
 
 app.get('/products', (req, res) => {
-  res.json([
-    {
-      name: 'Product 1',
-      price: 1000
-    },
-    {
-      name: 'Product 2',
-      price: 2000
-    },
-  ]
-  );
-})
+  const products = [];
+  const{size}=req.query;
+  const limit=size  || 10;
+  for (let index = 0; index < limit; index++) {
+    products.push({
+      name: faker.commerce.productName(),
+      price: parseInt(faker.commerce.price(), 10),
+      image: faker.image.url(),
 
+    });
+  }
+  res.json(products);
+});
+//todo lo que sea especifico ,debe ir antes de lo que es dinamico
+app.get('/products/filter',(req,res)=>{
+  res.send('yo soy un filter');
+});
+
+//cuando solo es un solo parametro ,solo se usa id/ 1.1
 app.get('/products/:id', (req, res) => {
   const { id } = req.params;
   res.json({
@@ -36,6 +45,21 @@ app.get('/products/:id', (req, res) => {
   });
 });
 
+app.get('/users', (req, res) => {
+  const { limit, offset } = req.query;
+  if (limit && offset) {
+    res.json({
+      limit,
+      offset
+    });
+  } else {
+    res.send('No hay parametros');
+  }
+})
+
+
+
+//cuando son muchos Id en la url,se pone con nombreId/ 1.1
 app.get('/categories/:categoryId/products/:productId', (req, res) => {
   const { categoryId, productId } = req.params;
   res.json({
@@ -43,6 +67,53 @@ app.get('/categories/:categoryId/products/:productId', (req, res) => {
     productId,
   });
 });
+
+
+//ejercicio
+app.get('/categories', (req, res) => {
+  res.json([
+    {
+      blue: true,
+      green: false,
+    },
+    {
+      blue: false,
+      red: true,
+    }
+  ])
+})
+//ejercicio
+app.get('/categories/:id', (req, res) => {
+  const { id } = req.params;
+  res.json({
+    id,
+    blue: true,
+    green: false,
+  })
+})
+//ejercicio
+app.get('/people', (req, res) => {
+  res.json([
+    {
+      name: 'jose',
+      type: 'employee'
+    },
+    {
+      name: 'katy',
+      type: 'customer'
+    }
+  ])
+})
+//ejercicio
+app.get('/people/:id', (req, res) => {
+  const { id } = req.params;
+  res.json({
+    id,
+    name: 'jose',
+    type: 'employee',
+  })
+})
+
 
 app.listen(port, () => {
   console.log('mi port' + port)
