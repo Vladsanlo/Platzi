7e1620f27b6df9228092a50028b4e651df41703b
Vladimir Sanchez <vladypuggu@gmail.com>
2025-02-15 19:56:58 -0600

    Created Products router
    Created users router
    
    Created categories router
    
    Created people router

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,20 @@
+const express = require('express');
+
 const productsRouter = require('./productsRouter')
 const usersRouter = require('./usersRouter')
 const categoriesRouter = require('./categoriesRouter')
 const peopleRouter = require('./peopleRouter')
 
 function routerApi(app) {
-  app.use('/products', productsRouter);
-  app.use('/users', usersRouter);
-  app.use('/categories', categoriesRouter);
-  app.use('/people', peopleRouter);
+  //path global   {
+  const router = express.Router();
+  app.use('/api/v1',router);
+  //  } sirve para "generalizar" una ruta,en este caso,todos van a usar /api/v1
+  router.use('/products', productsRouter);
+  router.use('/users', usersRouter);
+  router.use('/categories', categoriesRouter);
+  router.use('/people', peopleRouter);
 
 }
 
-moduele.exports = routerApi;
+module.exports = routerApi;
